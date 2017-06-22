584b0b3b8ac681b0370d8c5ee9a2bca9391d8e7a
Brian Frisch <brian@busycph.dk>
2017-06-22 22:07:00 +0200

    We've integrated firebase into this baby. Now snaps are saved to a cloudy environment.

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-const express = require('express')
-const app = express()
-const Raspistill = require('node-raspistill').Raspistill
+'use strict'
+
+require('dotenv').config()
+
+const admin = require('firebase-admin');
+const express = require('express');
+const app = express();
+const Raspistill = require('node-raspistill').Raspistill;
 const camera = new Raspistill({
     verticalFlip: true,
     width: 800,
     height: 600
-})
+});
+
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
 
 app.get('/snap', (req, res) => {
   camera.takePhoto().then((photo) => {
-    console.log(photo)
-  })
-})
+    const db = admin.database;
+    const ref = db.ref('server/saving-data/fridgecam');
+    
+    //const storage = admin.storage();
+    //const storageRef = storage.ref();
 
-app.get('*', (req, res) => {
-    res.status(404).send('Unrecognised API call')
-})
+    const snapsRef = ref.child('snaps');
+
+    photo = photo.toString('base64');
+
+    snapsRef.push({
+      snapper: "Brian Frisch",
+      time: Date.now(),
+      image: photo
+    }).then(() => {
+      res.status(200).send('<img src="'+photo+'">');
+    });
+  })
+});
 
 app.use((err, req, res, next) => {
   if (req.xhr) {
-    res.status(500).send('Oops, Something went wrong!')
+    res.status(500).send('Oops, Something went wrong!');
   } else {
-    next(err)
+    next(err);
   }
-})
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
